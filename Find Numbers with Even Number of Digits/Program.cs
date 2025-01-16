b50dff267ed2e75675bb50388d3bb24c7088fd8e
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-16 16:50:25 -0300

    New exercise added

--- Find Numbers with Even Number of Digits/Program.cs
+++ Find Numbers with Even Number of Digits/Program.cs
@@ -7,3 +7,33 @@ Given an array nums of integers, return how many of them contain an even number 
 LINK: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
 ----------------------------------------------------------------------
 */
+
+using System.Runtime.InteropServices;
+
+static int FindNumbers(int[] nums)
+{
+    int capacity = nums.Length;
+    int counter = default;
+    for (int i = 0; i < capacity; i++)
+    {
+        if (nums[i].ToString().Length % 2 == 0)
+            counter += 1;
+    }
+
+    return counter;
+}
+
+Random rn = new();
+int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rn.Next(1,10000);
+    System.Console.Write($" {array[i]} ");
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"La cantidad de numeros pares fueron: {FindNumbers(array)}");
+
+
+
+
---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+One-Dimensional Array 
+----------------------------------------------------------------------
+English:
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+LINK: https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3240/
+----------------------------------------------------------------------
+*/
